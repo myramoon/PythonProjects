f86bc8c0d764c49e96464744175e970e772fc686
anamfazal <anamfazal94@gmail.com>
2020-11-11 22:41:03 +0530

    Added: code to check whether employee is present

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -1,2 +1,11 @@
-print("Welcome to employee wage problem")
+import random
+
+IS_FULL_TIME = 1
+attendanceStatus = random.randint(0,1)   #checks whether employee is present
+if attendanceStatus == IS_FULL_TIME:
+    print("Employee is present")
+else:
+    print("Employee is absent")
+
+
 
