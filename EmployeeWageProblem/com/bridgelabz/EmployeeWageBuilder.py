3cdb60c9920d8024343e1fcbe668f56ecb8062db
anamfazal <anamfazal94@gmail.com>
2020-11-12 00:53:32 +0530

    Refactored: code to calculate monthly wages

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -1,20 +1,27 @@
 import random
 
+#Constants and Variables
 IS_FULL_TIME = 2
 IS_PART_TIME = 1
 EMP_RATE_PER_HOUR = 20
-attendanceStatus = random.randint(0,2)   #checks whether employee is present
+MAX_WORKING_DAYS = 20
+count = 0
+totalEmployeeWage = 0
+
+#Determines work hours
 def calculateWorkhours(attendanceStatus):
     switcher = {
-        IS_FULL_TIME: 8 ,
-        IS_PART_TIME: 4 ,
+        IS_FULL_TIME: 8,
+        IS_PART_TIME: 4,
     }
-    return switcher.get(attendanceStatus , 0)
+    return switcher.get(attendanceStatus, 0)
 
-workHours = calculateWorkhours(attendanceStatus) #gets calculated work hours
-employeeWage = workHours * EMP_RATE_PER_HOUR   #calculate wages
-print ("Employee wage for the day is: " , employeeWage)
+#Calculates monthly wages
+while count < MAX_WORKING_DAYS:
+    attendanceStatus = random.randint(0, 2)  # checks whether employee is present
+    workHours = calculateWorkhours(attendanceStatus)  # gets calculated work hours
+    employeeWage = workHours * EMP_RATE_PER_HOUR  # calculate wages
+    totalEmployeeWage += employeeWage
+    count += 1
 
-
-
-
+print("Employee wage for the month is: ", totalEmployeeWage)
