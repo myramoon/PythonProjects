355cb41b56a1c0753526655787166f440241a7d2
anamfazal <anamfazal94@gmail.com>
2020-11-11 23:09:30 +0530

    Added: code to calculate employee wage for a day

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -1,11 +1,15 @@
 import random
 
 IS_FULL_TIME = 1
+EMP_RATE_PER_HOUR = 20
 attendanceStatus = random.randint(0,1)   #checks whether employee is present
 if attendanceStatus == IS_FULL_TIME:
-    print("Employee is present")
+    workHours = 8
 else:
-    print("Employee is absent")
+    workHours = 0
+employeeWage = workHours * EMP_RATE_PER_HOUR   #calculate wages
+print ("Employee wage for the day is: " , employeeWage)
+
 
 
 
