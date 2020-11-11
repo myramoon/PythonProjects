2afbc8fba4434061e974a378bfcf7ba1f383d312
anamfazal <anamfazal94@gmail.com>
2020-11-12 00:31:42 +0530

    Refactored: code to calculate wages using dictionary

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -4,12 +4,14 @@ IS_FULL_TIME = 2
 IS_PART_TIME = 1
 EMP_RATE_PER_HOUR = 20
 attendanceStatus = random.randint(0,2)   #checks whether employee is present
-if attendanceStatus == IS_FULL_TIME:
-    workHours = 8
-elif attendanceStatus == IS_PART_TIME:
-    workHours = 4
-else:
-    workHours = 0
+def calculateWorkhours(attendanceStatus):
+    switcher = {
+        IS_FULL_TIME: 8 ,
+        IS_PART_TIME: 4 ,
+    }
+    return switcher.get(attendanceStatus , 0)
+
+workHours = calculateWorkhours(attendanceStatus) #gets calculated work hours
 employeeWage = workHours * EMP_RATE_PER_HOUR   #calculate wages
 print ("Employee wage for the day is: " , employeeWage)
 
