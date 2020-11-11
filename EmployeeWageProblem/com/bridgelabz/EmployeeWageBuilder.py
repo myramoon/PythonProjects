18dcc951d19eb8fc99796aca91bf9c455cc5b65b
anamfazal <anamfazal94@gmail.com>
2020-11-12 01:07:07 +0530

    Refactored: code to calculate monthly wages based on maximum hour and day conditions

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -1,14 +1,17 @@
 import random
 
-#Constants and Variables
+# Constants and Variables
 IS_FULL_TIME = 2
 IS_PART_TIME = 1
 EMP_RATE_PER_HOUR = 20
 MAX_WORKING_DAYS = 20
-count = 0
+MAX_MONTHLY_HOURS = 100
+dayCount = 0
 totalEmployeeWage = 0
+totalWorkHours = 0
 
-#Determines work hours
+
+# Determines work hours
 def calculateWorkhours(attendanceStatus):
     switcher = {
         IS_FULL_TIME: 8,
@@ -16,12 +19,14 @@ def calculateWorkhours(attendanceStatus):
     }
     return switcher.get(attendanceStatus, 0)
 
-#Calculates monthly wages
-while count < MAX_WORKING_DAYS:
+
+# Calculates monthly wages
+while dayCount < MAX_WORKING_DAYS and totalWorkHours <= MAX_MONTHLY_HOURS:
     attendanceStatus = random.randint(0, 2)  # checks whether employee is present
     workHours = calculateWorkhours(attendanceStatus)  # gets calculated work hours
-    employeeWage = workHours * EMP_RATE_PER_HOUR  # calculate wages
-    totalEmployeeWage += employeeWage
-    count += 1
+    dayCount += 1
+    totalWorkHours += workHours
+    print("Day number: ", dayCount, "  Emp Hr: ", workHours)
 
+totalEmployeeWage = totalWorkHours * EMP_RATE_PER_HOUR
 print("Employee wage for the month is: ", totalEmployeeWage)
