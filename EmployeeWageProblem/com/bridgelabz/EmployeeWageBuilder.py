99b7b249b4a7bcb4aa61ebc86dbb11d7a7f83900
anamfazal <anamfazal94@gmail.com>
2020-11-11 23:24:53 +0530

    Refactored: code to calculate part time employee wage

--- EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
+++ EmployeeWageProblem/com/bridgelabz/EmployeeWageBuilder.py
@@ -1,10 +1,13 @@
 import random
 
-IS_FULL_TIME = 1
+IS_FULL_TIME = 2
+IS_PART_TIME = 1
 EMP_RATE_PER_HOUR = 20
-attendanceStatus = random.randint(0,1)   #checks whether employee is present
+attendanceStatus = random.randint(0,2)   #checks whether employee is present
 if attendanceStatus == IS_FULL_TIME:
     workHours = 8
+elif attendanceStatus == IS_PART_TIME:
+    workHours = 4
 else:
     workHours = 0
 employeeWage = workHours * EMP_RATE_PER_HOUR   #calculate wages
