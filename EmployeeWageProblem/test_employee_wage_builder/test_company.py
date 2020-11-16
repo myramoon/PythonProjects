b30c1bd576f552fbc13d9524383515852f4b1431
anamfazal <anamfazal94@gmail.com>
2020-11-17 02:08:13 +0530

    Added: test case to verify employee wage when queried by company

--- EmployeeWageProblem/test_employee_wage_builder/test_company.py
+++ EmployeeWageProblem/test_employee_wage_builder/test_company.py
@@ -5,43 +5,30 @@ from EmployeeWageProblem.main_employee_wage_builder.employee_wage_calculator imp
 class Test_Employee_Wage_Builder:
 
     def test_given_company_details_should_match_company_name(self):
-        dmart = Company("Dmart", 20, 100, 20, 0)
-        assert dmart.name == "Dmart"
+        wage_builder = Employee_Wage_Calculator()
+        company = wage_builder.add_company_details("Dmart", 20, 100, 20, 0)
+        assert company.name == "Dmart"
 
     def test_given_company_details_should_match_company_max_work_days(self):
-        dmart = Company("Dmart", 20, 100, 20, 0)
-        assert dmart.max_working_days == 20
+        wage_builder = Employee_Wage_Calculator()
+        company = wage_builder.add_company_details("Dmart", 20, 100, 20, 0)
+        assert company.max_working_days == 20
 
     def test_given_company_details_should_match_company_max_work_hours(self):
-        dmart = Company("Dmart", 20, 100, 20, 0)
-        assert dmart.max_monthly_hrs == 100
-
-    def test_given_attendance_status_should_return_work_hours(self):
         wage_builder = Employee_Wage_Calculator()
-        assert wage_builder.calculate_work_hours(2) == 8
-
-    def test_given_company_details_should_calculate_employee_wage(self):
-        wage_builder = Employee_Wage_Calculator()
-        dmart = Company("Dmart", 20, 100, 20, 0)
-        reliance = Company("Reliance", 30, 80, 30, 0)
-        westside = Company("Westside", 15, 90, 30, 0)
-        wage_builder.calculate_employee_wage(dmart)
-        wage_builder.calculate_employee_wage(reliance)
-        wage_builder.calculate_employee_wage(westside)
-        assert True
+        company = wage_builder.add_company_details("Dmart", 20, 100, 20, 0)
+        assert company.max_monthly_hrs == 100
 
     def test_given_company_list_should_calculate_total_employee_wage_for_each_company(self):
-        companies = []
         wage_builder = Employee_Wage_Calculator()
-        dmart = Company("Dmart", 20, 100, 20, 0)
-        reliance = Company("Reliance", 30, 80, 30, 0)
-        westside = Company("Westside", 15, 90, 30, 0)
-        companies.append(dmart)
-        companies.append(reliance)
-        companies.append(westside)
-        for company in companies:
+        dmart = wage_builder.add_company_details("Dmart",  20,  100, 20,  0)
+        reliance = wage_builder.add_company_details("Reliance", 30,  80,  30, 0)
+        westside = wage_builder.add_company_details("Westside",  15,  90,  30,  0)
+        for company in Employee_Wage_Calculator.companies:
             wage_builder.calculate_employee_wage(company)
-        assert True
+        print(Employee_Wage_Calculator.company_wage_dictionary)
+        assert (wage_builder.get_total_wage(dmart) == Employee_Wage_Calculator.company_wage_dictionary.get('Dmart') )
+
 
 
 
